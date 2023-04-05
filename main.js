e181022eb6122f32f196ff5e5566fd9d2d846364
AkbarEverybody <akbarpribadi549@gmail.com>
2023-04-05 13:44:23 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const url = "https://icanhazdadjoke.com/";
+const teks = document.querySelector("#teks");
+const getJoke = async () => {
+  const dadJoke = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  try {
+    const dadJokeJSON = await dadJoke.json();
+    const data = await dadJokeJSON.joke;
+    if (dadJoke.status === 200) {
+      teks.innerText = data;
+    } else {
+      console.error("error");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+// (async () => console.log(await getDadJoke()))();
